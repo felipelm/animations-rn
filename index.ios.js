6e74a3ddf1814c5bc700fa888799dcd41da54840
“felipelm” <felipelm123@gmail.com>
2017-05-07 20:31:12 -0300

    adding reactions

--- index.ios.js
+++ index.ios.js
@@ -11,15 +11,16 @@ import {
   Text,
   View
 } from 'react-native';
-import Paralax from './app/paralax/paralax'
-import HeartView from './app/heart/heart-view'
-import ActionButtom from './app/action-buttom/action-buttom'
-import Swipe from './app/swipe'
+import Paralax from './app/paralax/paralax';
+import HeartView from './app/heart/heart-view';
+import ActionButtom from './app/action-buttom/action-buttom';
+import Swipe from './app/swipe';
+import HeartReaction from './app/heart-reaction';
 
 export default class animations extends Component {
   render() {
     return (
-      <Swipe />
+      <HeartReaction />
     );
   }
 }
