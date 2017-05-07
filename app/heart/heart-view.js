b54e8a56334ab31bbf6c87199d7c70e89490e26b
“felipelm” <felipelm123@gmail.com>
2017-05-07 17:21:18 -0300

    adding hearts animation

--- app/heart/heart-view.js
+++ app/heart/heart-view.js
@@ -10,6 +10,42 @@ import {
 } from 'react-native';
 import {Heart} from './heart'
 
+const getTransformationAnimation = (animation, scale, y, x, rotate, opacity) => {
+  const scaleAnimation = animation.interpolate({
+    inputRange: [0,1],
+    outputRange: [0,scale]
+  })
+
+  const xAnimation = animation.interpolate({
+    inputRange: [0,1],
+    outputRange: [0, x]
+  })
+
+  const yAnimation = animation.interpolate({
+    inputRange: [0,1],
+    outputRange: [0, y]
+  })
+
+  const rotateAnimation = animation.interpolate({
+    inputRange: [0,1],
+    outputRange: ['0deg', rotate]
+  })
+
+  const opacityAnimation = animation.interpolate({
+    inputRange: [0,1],
+    outputRange: [0, opacity]
+  })
+  return {
+    opacity: opacityAnimation,
+    transform: [
+      {scale: scaleAnimation},
+      {translateX: xAnimation},
+      {translateY: yAnimation},
+      {rotate: rotateAnimation}
+    ]
+  }
+}
+
 export default class HeartView extends Component {
   constructor(props) {
     super(props);
@@ -32,11 +68,30 @@ export default class HeartView extends Component {
     this.setState({
       liked: !this.state.liked
     })
-    Animated.spring(this.state.scale, {
-      toValue: 2,
-      friction: 3
-    }).start(() => {
-      this.state.scale.setValue(0);
+    const showAnimations = this.state.animations.map((animation) => {
+      return Animated.spring(animation, {
+        toValue: 1,
+        friction: 4
+      })
+    })
+    const hideAnimations = this.state.animations.map((animation) => {
+      return Animated.timing(animation, {
+        toValue: 0,
+        duration: 50
+      })
+    }).reverse();
+    Animated.parallel([
+      Animated.spring(this.state.scale, {
+        toValue: 2,
+        friction: 3
+      }),
+      Animated.sequence([
+        Animated.stagger(50, showAnimations),
+        Animated.delay(100),
+        Animated.stagger(50, hideAnimations)
+      ])
+    ]).start(() => {
+      this.state.scale.setValue(0)
     })
   }
 
@@ -53,6 +108,12 @@ export default class HeartView extends Component {
     return (
       <View style={styles.container}>
         <View>
+          <Heart filled style={[styles.heart, getTransformationAnimation(this.state.animations[5],.4,-280,0,"10deg",.7)]}/>
+          <Heart filled style={[styles.heart, getTransformationAnimation(this.state.animations[4],.7,-120,40,"45deg",.5)]}/>
+          <Heart filled style={[styles.heart, getTransformationAnimation(this.state.animations[3],.8,-120,-40,"-45deg",.3)]}/>
+          <Heart filled style={[styles.heart, getTransformationAnimation(this.state.animations[2],.3,-150,120,"-35deg",.6)]}/>
+          <Heart filled style={[styles.heart, getTransformationAnimation(this.state.animations[1],.3,-120,-120,"-35deg",.7)]}/>
+          <Heart filled style={[styles.heart, getTransformationAnimation(this.state.animations[0],.8,-60,0,"35deg",.8)]}/>
           <TouchableWithoutFeedback onPress={this.triggerLike}>
             <Animated.View style={heartButtonStyle}>
               <Heart filled={this.state.liked}/>
@@ -70,4 +131,9 @@ const styles = StyleSheet.create({
     justifyContent: "center",
     alignItems: "center"
   },
+  heart: {
+    position: "absolute",
+    top: 0,
+    left: 0
+  }
 });
